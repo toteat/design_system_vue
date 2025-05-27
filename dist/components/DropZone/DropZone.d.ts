84ba8032149c8e365cf5bb895b4a03eeb556b80d
Mauricio Antúnez <mantunez@toteat.com>
2025-05-27 12:06:47 -0400

    improved DropZone component

--- dist/components/DropZone/DropZone.d.ts
+++ dist/components/DropZone/DropZone.d.ts
@@ -1,12 +1,47 @@
-import { DropZoneProps, FileWithPreview } from '../../types';
-declare const _default: import('vue').DefineComponent<DropZoneProps, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {} & {
+import { FileList, DropZoneProps, FileWithPreview } from '../../types';
+type __VLS_Props = DropZoneProps & {
+    modelValue?: FileWithPreview[];
+};
+declare const _default: import('vue').DefineComponent<__VLS_Props, {
+    /**
+     * Get all currently processed files
+     * @returns Array of FileWithPreview
+     */
+    getFiles: () => {
+        file: {
+            readonly lastModified: number;
+            readonly name: string;
+            readonly webkitRelativePath: string;
+            readonly size: number;
+            readonly type: string;
+            arrayBuffer: () => Promise<ArrayBuffer>;
+            bytes: () => Promise<Uint8Array>;
+            slice: (start?: number, end?: number, contentType?: string) => Blob;
+            stream: () => ReadableStream<Uint8Array>;
+            text: () => Promise<string>;
+        };
+        name: string;
+        preview: string;
+    }[];
+    /**
+     * Clear all files
+     */
+    clearFiles: () => void;
+    /**
+     * Add files programmatically
+     * @param files FileList or File[]
+     */
+    addFiles: (files: FileList | globalThis.File[]) => Promise<void>;
+}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {} & {
     [x: `${string}-drop`]: (files: globalThis.FileList) => any;
     [x: `${string}-drop-error`]: (message: string) => any;
     [x: `${string}-remove`]: (file: FileWithPreview) => any;
-}, string, import('vue').PublicProps, Readonly<DropZoneProps> & Readonly<{
+    "update:modelValue": (files: FileWithPreview[]) => any;
+}, string, import('vue').PublicProps, Readonly<__VLS_Props> & Readonly<{
     [x: `on${Capitalize<string>}-drop`]: ((files: globalThis.FileList) => any) | undefined;
     [x: `on${Capitalize<string>}-drop-error`]: ((message: string) => any) | undefined;
     [x: `on${Capitalize<string>}-remove`]: ((file: FileWithPreview) => any) | undefined;
+    "onUpdate:modelValue"?: ((files: FileWithPreview[]) => any) | undefined;
 }>, {
     label: string;
     allowedFileTypes: import('../../types').AllowedFileTypes;
@@ -14,6 +49,7 @@ declare const _default: import('vue').DefineComponent<DropZoneProps, {}, {}, {},
     accept: string;
     displayPreview: boolean;
     displayFileList: boolean;
+    modelValue: FileWithPreview[];
 }, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {
     inputRef: globalThis.HTMLInputElement;
 }, any>;
